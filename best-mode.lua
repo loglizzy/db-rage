e54fc2cd0888a89b922876a18c274b25d54010df
loglizzy <72479668+loglizzy@users.noreply.github.com>
2021-03-16 23:21:40 -0400

    Update best-mode.lua

--- best-mode.lua
+++ best-mode.lua
@@ -25,10 +25,10 @@ local function main()
         if n and m then
             if l then
                 if l[2] < m and s({v[2],v[3],v[4],v[5]}) then
-                    l = {n, i+1}
+                    l = {n, m, i}
                 end
             elseif s({v[2],v[3],v[4],v[5]}) then
-                l = {n, i+1}
+                l = {n, m, i}
             end
         end
     end
