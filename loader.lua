846d1896c2247f7e3fda2f29f2e1515d8727b652
loglizzy <72479668+loglizzy@users.noreply.github.com>
2021-03-16 23:46:44 -0400

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,21 @@
+repeat wait() until game:IsLoaded() end
+
+local services = {}
+
+local success, err = pcall(function()
+    local requirements = {
+    'https://raw.githubusercontent.com/loglizzy/db-rage/main/best-mode.lua'
+}
+    
+    for i,v in pairs(requirements) do
+        local content = loadstring(game:HttpGet(v))()
+        
+        services[content.Name] = content.Function
+    end
+end)
+
+if succcess then
+  return requirements
+else
+  return error('Loading failed')
+end
