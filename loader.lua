b611ce2892f247c32a62de8c91751401ad9e1d21
loglizzy <72479668+loglizzy@users.noreply.github.com>
2021-03-17 00:32:53 -0400

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -2,7 +2,7 @@ repeat wait() until game:IsLoaded()
 local services = {}
 
 local requirements = {
-    'https://raw.githubusercontent.com/loglizzy/db-rage/main/best-mode.lua'
+    'https://raw.githubusercontent.com/loglizzy/db-rage/main/modules/best-mode.lua'
 }
 for i,v in pairs(requirements) do
     local content = loadstring(game:HttpGet(v))()
